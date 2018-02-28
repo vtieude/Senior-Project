013d4ff89cb0ce97cfdff5ba6df3764c5e8207c0
Wilson <wilson@enclave.vn>
2018-02-28 16:01:00 +0700

    Create project

---
+++ app/src/main/java/com/example/wilson/humancharacteristics/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.wilson.humancharacteristics;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.wilson.humancharacteristics.CameraDetect.CameraDetectActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        Button btStart = (Button)this.findViewById(R.id.button);
+        btStart.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                // Tạo một Intent:
+                // (Mang nội dung sẽ gửi tới Example1Activity).
+                Intent myIntent = new Intent(MainActivity.this, CameraDetectActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+}
---
+++ app/src/main/java/com/example/wilson/humancharacteristics/Setting/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.example.wilson.humancharacteristics.Setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wilson.humancharacteristics.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
